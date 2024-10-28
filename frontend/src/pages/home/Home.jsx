c4c798c16ec2470d28d5b2310447ba4afbff6864
RAmiHomsi <ramihomsi1997@hotmail.com>
2024-10-28 21:24:38 +0100

    first commit

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "./Banner";
+import TopSellers from "./TopSellers";
+import News from "./News";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <TopSellers />
+      <News />
+    </>
+  );
+};
+
+export default Home;
